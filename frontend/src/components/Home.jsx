229839b759b33d189f8e188a878e62bbeeadcb89
Akhilesh1326 <akhileshpimple3@gmail.com>
2024-06-25 20:51:22 +0530

    add component folder create Main.jsx file

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const Home = () => {
   return (
     <div>
-      hello
+      <h1>helo</h1>
     </div>
   )
 }
 
-export default App
+export default Home
