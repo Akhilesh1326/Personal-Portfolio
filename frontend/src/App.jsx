2d10e7305391783c8ccf46471e08ab3400624d9e
Akhilesh26 <akhileshpimple3@gmail.com>
2024-10-11 20:47:51 +0530

    remove home add main and use it for new portfolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Home from './components/Home'
+import Main from './components/Main'
 const App = () => {
   return (
     <>
-    <Home/>
+    <Main/>
     </>
 
   )
